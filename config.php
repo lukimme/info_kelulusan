41f6ab742ddc5532826b7fa4f9cecf511f049265
yuktiid <go.yukti.id@gmail.com>
2025-05-03 20:16:41 +0700

    db

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 
 //offline
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'lulusan';
+// $host = 'localhost';
+// $user = 'root';
+// $pass = '';
+// $db = 'lulusan';
 
 //online
-// $host = 'localhost';
-// $user = 'u1583982_lulusan';
-// $pass = 'i7fc%6ufwe87tEeiyu2wtbw9';
-// $db = 'u1583982_lulusan';
+$host = 'localhost';
+$user = 'u1583982_lulusan';
+$pass = 'i7fc%6ufwe87tEeiyu2wtbw9';
+$db = 'u1583982_lulusan';
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) { die('Koneksi gagal: ' . $conn->connect_error); }
